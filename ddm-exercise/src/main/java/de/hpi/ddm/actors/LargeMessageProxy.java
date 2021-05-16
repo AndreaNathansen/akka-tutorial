6a01c328ca86a182df99f3deffc1ae63d5c942d8
Martin Buessemeyer <martin.buessemeyer@student.hpi.de>
2021-05-16 17:53:00 +0200

    solve task 2

--- ddm-exercise/src/main/java/de/hpi/ddm/actors/LargeMessageProxy.java
+++ ddm-exercise/src/main/java/de/hpi/ddm/actors/LargeMessageProxy.java
@@ -1,6 +1,9 @@
 package de.hpi.ddm.actors;
 
-import java.io.Serializable;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
 import akka.actor.AbstractLoggingActor;
 import akka.actor.ActorRef;
@@ -17,6 +20,9 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 	////////////////////////
 
 	public static final String DEFAULT_NAME = "largeMessageProxy";
+
+	static int messageSize = 6 * 1024 * 1024;
+	private int messageIDCounter= 0;
 	
 	public static Props props() {
 		return Props.create(LargeMessageProxy.class);
@@ -40,7 +46,25 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 		private ActorRef sender;
 		private ActorRef receiver;
 	}
-	
+
+	@Data @NoArgsConstructor @AllArgsConstructor
+	public static class BytesPartMessage implements Serializable {
+		private static final long serialVersionUID = 10000000000000000L;
+		private byte[] bytes;
+		private String messageId;
+		private int numChunks;
+		private int chunkId;
+		private ActorRef sender;
+		private ActorRef receiver;
+	}
+
+	private String getMessageID(){
+		return this.self().toString() + this.messageIDCounter++;
+	}
+
+	private HashMap<String, byte [][]> receivedBytePartLargeMessages = new HashMap<>();
+	private HashMap<String, Integer> numBytesReceived = new HashMap<>();
+
 	/////////////////
 	// Actor State //
 	/////////////////
@@ -57,13 +81,12 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 	public Receive createReceive() {
 		return receiveBuilder()
 				.match(LargeMessage.class, this::handle)
-				.match(BytesMessage.class, this::handle)
+				.match(BytesPartMessage.class, this::handle)
 				.matchAny(object -> this.log().info("Received unknown message: \"{}\"", object.toString()))
 				.build();
 	}
 
-	private void handle(LargeMessage<?> largeMessage) {
-		Object message = largeMessage.getMessage();
+	private void handle(LargeMessage<?> largeMessage) throws IOException {
 		ActorRef sender = this.sender();
 		ActorRef receiver = largeMessage.getReceiver();
 		ActorSelection receiverProxy = this.context().actorSelection(receiver.path().child(DEFAULT_NAME));
@@ -80,12 +103,66 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 		// - To split an object, serialize it into a byte array and then send the byte array range-by-range (tip: try "KryoPoolSingleton.get()").
 		// - If you serialize a message manually and send it, it will, of course, be serialized again by Akka's message passing subsystem.
 		// - But: Good, language-dependent serializers (such as kryo) are aware of byte arrays so that their serialization is very effective w.r.t. serialization time and size of serialized data.
-		receiverProxy.tell(new BytesMessage<>(message, sender, receiver), this.self());
+		byte[] serializedMessage = new byte[0];
+		try (ByteArrayOutputStream bos = new ByteArrayOutputStream()) {
+			ObjectOutputStream oos = new ObjectOutputStream(bos);
+			oos.writeObject(largeMessage.getMessage());
+			oos.flush();
+			serializedMessage = bos.toByteArray();
+		}
+		int chunkID = 0;
+		int numChunks =  (int) Math.ceil(serializedMessage.length / (double)messageSize);
+		for (int i = 0; i < serializedMessage.length; i += messageSize){
+			byte[] msg = Arrays.copyOfRange(serializedMessage, i, Math.min(i + messageSize, serializedMessage.length));
+			receiverProxy.tell(new BytesPartMessage(msg, this.getMessageID(), numChunks, chunkID, sender, receiver), this.self());
+			chunkID++;
+		}
 	}
 
-	private void handle(BytesMessage<?> message) {
+	/*private void handle(BytesMessage<?> message) {
 		// TODO: With option a): Store the message, ask for the next chunk and, if all chunks are present, reassemble the message's content, deserialize it and pass it to the receiver.
 		// The following code assumes that the transmitted bytes are the original message, which they shouldn't be in your proper implementation ;-)
 		message.getReceiver().tell(message.getBytes(), message.getSender());
+	}*/
+
+	// Let it crash.
+	private void handle(BytesPartMessage message) throws IOException, ClassNotFoundException {
+		// New message.
+		String messageId = message.getMessageId();
+		if (!receivedBytePartLargeMessages.containsKey(messageId)) {
+			receivedBytePartLargeMessages.put(messageId, new byte[message.getNumChunks()][]);
+			numBytesReceived.put(messageId, 0);
+		}
+		byte[][] receivedBytePartMessages = receivedBytePartLargeMessages.get(messageId);
+		receivedBytePartMessages[message.getChunkId()] = message.getBytes();
+		Integer numBytesReceivedForMessage = numBytesReceived.get(messageId);
+
+		numBytesReceivedForMessage++;
+		// Received whole message.
+		if (numBytesReceivedForMessage == receivedBytePartMessages.length){
+			// Get large message byte size.
+			int msgSize = 0;
+			for (byte[] bytePartMessage : receivedBytePartMessages) {
+				msgSize += bytePartMessage.length;
+			}
+			// Copy two dimensional large message to one dimensional array.
+			byte [] largeMessageBytes = new byte[msgSize];
+			int index = 0;
+			for (byte[] receivedBytePartMessage : receivedBytePartMessages) {
+				for (byte b : receivedBytePartMessage) {
+					largeMessageBytes[index++] = b;
+				}
+			}
+			// Deserialize object.
+			ByteArrayInputStream bis = new ByteArrayInputStream(largeMessageBytes);
+			ObjectInputStream ois = new ObjectInputStream(bis);
+			Object sendObject = ois.readObject();
+
+			// Reset internal message buffer.
+			receivedBytePartLargeMessages.remove(messageId);
+			numBytesReceived.remove(messageId);
+			// Send large message.
+			message.getReceiver().tell(sendObject, message.getSender());
+		}
 	}
 }
